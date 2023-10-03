b2e47d379c11885c4bf1b92ea83202a690863385
Patryk Wikłacz <wiklacz1patryk@gmail.com>
2023-10-03 16:19:42 +0200

    added routing to navbar

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -13,6 +13,10 @@ export class ShopService {
   baseUrl = 'https://localhost:5209/api/';
   constructor(private http: HttpClient) { }
 
+  getProduct(id: number) {
+    return this.http.get<Product>(this.baseUrl + 'products/' + id)
+  }
+
   getProducts(ShopParams: ShopParams){
     let params = new HttpParams();
 
