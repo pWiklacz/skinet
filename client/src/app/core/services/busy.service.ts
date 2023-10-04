9d106eeb2bc7572409c344b75798fc60a64e0be6
Patryk Wikłacz <wiklacz1patryk@gmail.com>
2023-10-04 12:43:46 +0200

    Added home page content, added loading indicator, added breadcrumb

---
+++ client/src/app/core/services/busy.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BusyService {
+  busyRequesCount = 0;
+  constructor(private spinnerService: NgxSpinnerService) { }
+
+  busy() {
+    this.busyRequesCount++;
+    this.spinnerService.show(undefined, {
+      type: 'timer',
+      bdColor: 'rgba(255,255,255,0.7)',
+      color: '#333333'
+    })
+  }
+
+  idle() {
+    this.busyRequesCount--;
+    if (this.busyRequesCount <= 0) {
+      this.busyRequesCount = 0;
+      this.spinnerService.hide();
+    }
+  }
+}
