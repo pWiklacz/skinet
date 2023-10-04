7b96b630e90e06b501e0f1ed553fc156d0b19369
Patryk Wikłacz <wiklacz1patryk@gmail.com>
2023-10-04 20:52:26 +0200

    Added cart functionality. Added checkout view. Updated produtc detail view

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Pagination } from './shared/models/pagination';
 import { Product } from './shared/models/product';
+import { BasketService } from './basket/basket.service';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,11 @@ import { Product } from './shared/models/product';
 })
 export class AppComponent implements OnInit {
   title = 'Skinet';
-  constructor() {}
+
+  constructor(private basketService: BasketService) {}
+
   ngOnInit(): void {
+    const basketId = localStorage.getItem('basket_id');
+    if(basketId) this.basketService.getBasket(basketId);
   }
 }
