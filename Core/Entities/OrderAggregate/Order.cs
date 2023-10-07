4d96e4742350e5d58e5bf06c4190f0b1b0013b00
Patryk Wikłacz <patrwik434@student.polsl.pl>
2023-10-07 23:54:17 +0200

    Added classes for order aggregate

---
+++ Core/Entities/OrderAggregate/Order.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Entities.OrderAggregate
+{
+    public class Order
+    {
+        
+    }
+}
